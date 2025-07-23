099c37d3b6b376f3ceccb08815e382bd8b30035a
otegami <otegami@clear-code.com>
2025-07-23 16:58:38 +0900

    components/ElectricalTable: fix styles

--- website/src/components/ElectricalTable.tsx
+++ website/src/components/ElectricalTable.tsx
@@ -37,18 +37,17 @@ const components: ElectricalComponent[] = [
   { name: 'Extension Cable', image: 'extension.png', model: <a href="/files/power_extension.pdf" target="_blank" rel="noopener noreferrer">Extension Cable Drawing</a>, quantity: 2, unitPrice: 197, supplier: 'LCSC'},
   { name: 'Power Supply', image: 'power-supply.jpg', model: <a href="https://www.aliexpress.com/item/1005004204524395.html" target="_blank" rel="noopener noreferrer">Power Supply Purchase Link</a>, quantity: 2, unitPrice: 14634, supplier: 'AliExpress'},
   { name: 'PCB', image: 'pcb.png', model: (
-  <>
-    <div>
-      <a href="/files/Gerber_For_Hub.zip" target="_blank" rel="noopener noreferrer">Gerber Zip File</a>
-    </div>
-    <div>
-      <a href="/files/BOM_For_Hub.csv" target="_blank" rel="noopener noreferrer">BOM CSV file</a>
-    </div>
-    <div>
-      <a href="/files/CPL_For_Hub.xlsx" target="_blank" rel="noopener noreferrer">CPL xlsx file</a>
-    </div>
-  </>
-), quantity: 2, unitPrice: 520, supplier: 'JLCPCB'},
+    <>
+      <div>
+        <a href="/files/Gerber_For_Hub.zip" target="_blank" rel="noopener noreferrer">Gerber Zip File</a>
+      </div>
+      <div>
+        <a href="/files/BOM_For_Hub.csv" target="_blank" rel="noopener noreferrer">BOM CSV file</a>
+      </div>
+      <div>
+        <a href="/files/CPL_For_Hub.xlsx" target="_blank" rel="noopener noreferrer">CPL xlsx file</a>
+      </div>
+    </>), quantity: 2, unitPrice: 520, supplier: 'JLCPCB'},
   { name: 'Connector', image: 'connector.jpg', model: 'C19268029 (do not need to purchase separately if using the LCSC PCB Assembly option)', quantity: 12, unitPrice: 60, supplier: 'LCSC'},
   { name: 'Emergency Stop', image: 'estop.jpg', model: <a href="https://www.monotaro.com/p/6001/0711/" target="_blank" rel="noopener noreferrer">Emergency Stop Purchase Link</a>, quantity: 1, unitPrice: 4698, supplier: 'Monotaro'},
 ];
@@ -56,11 +55,7 @@ const components: ElectricalComponent[] = [
 const columns: BoMTableColumn<ElectricalComponent>[] = [
   { header: 'Name', key: 'name' },
   { header: 'Photo', key: 'image' },
-  {
-    header: 'Resource',
-    key: 'model',
-    render: (value) => value // This lets ReactNode like <a> render directly
-  },
+  { header: 'Resource', key: 'model' },
   { header: 'Quantity', key: 'quantity' },
   { header: 'Unit Price (JPY)', key: 'unitPrice' },
   { header: 'Total Price (JPY)', key: 'totalPrice' },
