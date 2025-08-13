be4fe623fef1acd605db72dedb33ca4324eca7bf
otegami <otegami@clear-code.com>
2025-08-13 11:14:35 +0900

    scripts: remove unnecessary `?` because the reactionGroups is always there

--- website/scripts/fetch-popular-issues.sh
+++ website/scripts/fetch-popular-issues.sh
@@ -22,5 +22,5 @@ gh issue list \
   --repo enactic/openarm \
   --search "is:issue state:open sort:reactions-+1-desc type:Feature reactions:>=1" \
   --json author,number,reactionGroups,title,updatedAt,url --limit 20 | \
-jq '[.[] | select(.reactionGroups[]? | select(.content == "THUMBS_UP" and .users.totalCount > 0))] | .[0:5]' \
+jq '[.[] | select(.reactionGroups[] | select(.content == "THUMBS_UP" and .users.totalCount > 0))] | .[0:5]' \
   > static/data/popular-issues.json
